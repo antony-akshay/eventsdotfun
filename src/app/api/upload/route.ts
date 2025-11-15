788f24e22c269b61545ced0a9ffc4902001f67a4
antony-akshay <akshay.dev.ac.in@gmail.com>
2025-11-15 23:10:03 +0530

    extends class integration working

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,12 +1,9 @@
 export async function POST(req: Request) {
 
   interface JsonBinResponse {
-    record: any;
-    metadata: {
-      id: string;
-      createdAt: string;
-      private: boolean;
-    };
+    status:number;
+    uri:string;
+    id:string;
   }
 
 
@@ -42,28 +39,44 @@ export async function POST(req: Request) {
 
     console.log("jsonString:", jsonString);
 
-    const jsonres = await fetch("https://api.jsonbin.io/v3/b", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Master-Key": "$2a$10$iFJtjFvDRehOzcAc7XxWYuI1.0cGl6v.I9pShwQCrzp/uQNTn5I6O",
-        "X-Access-Key": "$2a$10$M/H1k5GZ1Um5QtoJiqyLM.n4uZ2yqkv7C3qaJaRiYPvmV5FYP3x/G"
+    // const jsonres = await fetch("https://api.jsonbin.io/v3/b", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     "X-Master-Key": "$2a$10$iFJtjFvDRehOzcAc7XxWYuI1.0cGl6v.I9pShwQCrzp/uQNTn5I6O",
+    //     "X-Access-Key": "$2a$10$M/H1k5GZ1Um5QtoJiqyLM.n4uZ2yqkv7C3qaJaRiYPvmV5FYP3x/G"
+    //   },
+    //   body: jsonString
+    // });
+
+    // console.log("jsonres : ", jsonres);
+
+    // const data: JsonBinResponse = await jsonres.json();
+    // const binId: string = data.metadata.id;
+
+    // console.log("binId :  ", binId);
+
+    // const finalUrl = `https://api.jsonbin.io/v3/b/${binId}?meta=false`;
+
+    // console.log("finalUrl : ",finalUrl);
+
+
+    //extendsclass implementation
+    const jsonres = await fetch("https://json.extendsclass.com/bin",{
+      method:"POST",
+      headers:{
+        "Api-key":"3b1bd95b-c247-11f0-9924-0242ac110008",
       },
-      body: jsonString
+      body:jsonString
     });
 
     console.log("jsonres : ", jsonres);
 
     const data: JsonBinResponse = await jsonres.json();
-    const binId: string = data.metadata.id;
 
-    console.log("binId :  ", binId);
+    console.log("url:",data.uri);
 
-    const finalUrl = `https://api.jsonbin.io/v3/b/${binId}?meta=false`;
-
-    console.log("finalUrl : ",finalUrl);
-
-    return new Response(JSON.stringify({ url: text, finalUrl: finalUrl }), {
+    return new Response(JSON.stringify({ url: text}), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
